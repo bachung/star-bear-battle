9f920db55a7610a19b41f12c8a1ffec25ccf92ae
bachung <bachung@gmail.com>
2024-07-14 22:29:22 -0700

    Anti aliasing

--- starbearbattle.js
+++ starbearbattle.js
@@ -1,15 +1,33 @@
-const STAR_BEAR_URL =
-  "https://www.gstatic.com/android/keyboard/emojikitchen/20210831/u1f43b/u1f43b_u2b50.png";
-const OFFSET = -0.3332889870078079;
-const SIZE = OFFSET * -2;
+(() => {
+  const STAR_BEAR_URL =
+    "https://www.gstatic.com/android/keyboard/emojikitchen/20210831/u1f43b/u1f43b_u2b50.png";
+  const OFFSET = -0.3332889870078079;
+  const SIZE = OFFSET * -2;
 
-const image = document.createElement("img");
-image.src = STAR_BEAR_URL;
+  const image = document.createElement("img");
+  image.src = STAR_BEAR_URL;
 
-image.onload = () => {
-  window.trace_star = (gDC, cw) => {
-    gDC.drawImage(image, OFFSET * cw, OFFSET * cw, SIZE * cw, SIZE * cw);
+  // https://stackoverflow.com/questions/17861447/html5-canvas-drawimage-how-to-apply-antialiasing
+  const antiAliasingCanvas = document.createElement("canvas");
+
+  image.onload = () => {
+    antiAliasingCanvas.width = image.width;
+    antiAliasingCanvas.height = image.height;
+    const antiAliasingContext = antiAliasingCanvas.getContext("2d");
+    const steps = 3;
+    antiAliasingContext.filter = `blur(${steps}px)`;
+    antiAliasingContext.drawImage(image, 0, 0);
+
+    window.trace_star = (gDC, cw) => {
+      gDC.drawImage(
+        antiAliasingCanvas,
+        OFFSET * cw,
+        OFFSET * cw,
+        SIZE * cw,
+        SIZE * cw
+      );
+    };
+
+    refreshCanvas();
   };
-
-  refreshCanvas();
-};
+})();
